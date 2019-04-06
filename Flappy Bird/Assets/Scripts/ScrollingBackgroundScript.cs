996db8696463d5f8b689250172501326d793a14d
Unknown <karol.marszalik@gmail.com>
2019-04-06 19:36:14 +0200

    Added Scrolling Background

---
+++ Flappy Bird/Assets/Scripts/ScrollingBackgroundScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackgroundScript : MonoBehaviour
+{
+    [SerializeField] private float bgSpeed = 1f;
+    [SerializeField] private Renderer bgRenderer;
+
+    private void Update()
+    {
+        bgRenderer.material.mainTextureOffset += new Vector2(bgSpeed * Time.deltaTime, 0f);
+    }
+}
